0a6e8d1bb9fd46161df36ca8b99b9775bd51abe3
EDRV666 <edrova04@gmail.com>
2024-10-30 15:27:40 -0600

    cambios

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors  = require('cors');
+
+
 
 //cargar variables de entorno
 require('dotenv').config();
@@ -23,6 +26,7 @@ const app = express();
 const PORT = 3000;
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
